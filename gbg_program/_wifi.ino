cc3fb440a27cb44915e8e21aad28cd48aca40d72
joshua-8 <joshua_phelps@brown.edu>
2024-09-21 11:46:42 -0700

    better html processing in progress

--- gbg_program/_wifi.ino
+++ gbg_program/_wifi.ino
@@ -8,19 +8,6 @@
 IPAddress apIP(10, 0, 0, 1);
 WebServer webServer(80);
 
-// website:
-/*
-  <!DOCTYPE html><html lang='en'><head>
-  <title>GBG CAR</title></head><body>
-  <p>Use the buttons below to turn the car's joystick on and off.</p>
-  <button onclick='fetch(\"/activate\");' style='font-size: 100px;'>GO</button>
-  <button onclick='fetch(\"/deactivate\");' style='font-size: 200px; float: right;'>STOP</button>
-  </body></html>
-*/
-// run through a html minifier (like this one https://kangax.github.io/html-minifier/) to make it one line, and save memory. Then put it in indexHTML
-
-const char indexHTML[] = "<!doctypehtml><html lang=en><title>GBG CAR</title><p>Use the buttons below to turn the car's joystick on and off.</p><button onclick='fetch(\"/activate\");'style=font-size:100px;'>GO</button> <button onclick='fetch(\"/deactivate\");'style=font-size:200px;float:right;'>STOP</button>";
-
 void setupWifi() {
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
