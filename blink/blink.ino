5b6fe79f6b9a49ccbd1b9b17367a0c23f14ada77
joshua-8 <joshua_phelps@brown.edu>
2024-09-22 11:05:38 -0700

    Update blink.ino

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,10 +1,13 @@
+#ifdef ESP32
+#define LED_BUILTIN 2
+#endif
 void setup(){
-    pinMode(LED_BUILTIN,OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop(){
-    digitalWrite(LED_BUILTIN,HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
     delay(250);
-    digitalWrite(LED_BUILTIN,LOW);
+    digitalWrite(LED_BUILTIN, LOW);
     delay(750);
 }
 
