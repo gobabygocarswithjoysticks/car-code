cfb4dc1ac48f3832b882a2435f81fd7ea8ad18fb
joshua-8 <joshua_phelps@brown.edu>
2024-09-22 14:31:01 -0700

    Update clear_eeprom.ino

--- clear_eeprom/clear_eeprom.ino
+++ clear_eeprom/clear_eeprom.ino
@@ -5,15 +5,16 @@
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-#if defined(ARDUINO_ARCH_MBED_RP2040)|| defined(ARDUINO_ARCH_RP2040)
+#if defined(ARDUINO_ARCH_MBED_RP2040) || defined(ARDUINO_ARCH_RP2040) || defined(ESP32)
   EEPROM.begin(1024);
+
 #endif
   for (int i = 0 ; i < EEPROM.length() ; i++) {
     EEPROM.write(i, 255);
     delay(1);
   }
-#if defined(ARDUINO_ARCH_MBED_RP2040)|| defined(ARDUINO_ARCH_RP2040)
-  EEPROM.commit(); // rp2040 EEPROM library requires this to be used to write the updated data to the flash that is simulating EEPROM (flash has more limited cycles)
+#if defined(ARDUINO_ARCH_MBED_RP2040)|| defined(ARDUINO_ARCH_RP2040) || defined(ESP32)
+  EEPROM.commit(); // rp2040 and esp32 EEPROM libraries require this to be used to write the updated data to the flash that is simulating EEPROM (flash has more limited cycles)
 #endif
   digitalWrite(LED_BUILTIN, LOW);
 }
