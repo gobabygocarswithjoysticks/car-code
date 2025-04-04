17b6579af4dd599ef24ac7f5a8165ecfc9d84182
joshua-8 <joshua262144@gmail.com>
2025-04-04 01:00:57 -0700

    centralize settings names to save memory

--- gbg_program/_Save_Recall.ino
+++ gbg_program/_Save_Recall.ino
@@ -41,6 +41,8 @@ void settingsMemory()
   recallSettings();
 }
 
+#include "settings_names.h"
+
 void settingsSerial() {
   int8_t in = Serial.read();
   if (in != -1) {
@@ -51,107 +53,107 @@ void settingsSerial() {
       char *v = strtok(0, ":,");
       boolean changedSomething = true;  // used to see if a valid command was found
       memset(resultBuf, '\0', 15);
-      if (strcmp(k, "CONTROL_RIGHT") == 0) {
+      if(strcmp_P(k, SETTING[S_CONTROL_RIGHT])==0) {
         CONTROL_RIGHT = atoi(v);
         sprintf(resultBuf, "%d", CONTROL_RIGHT);
-      } else if (strcmp(k, "CONTROL_CENTER_X") == 0) {
+      } else if(strcmp_P(k, SETTING[S_CONTROL_CENTER_X])==0) {
         CONTROL_CENTER_X = atoi(v);
         sprintf(resultBuf, "%d", CONTROL_CENTER_X);
-      } else if (strcmp(k, "CONTROL_LEFT") == 0) {
+      } else if(strcmp_P(k, SETTING[S_CONTROL_LEFT])==0) {
         CONTROL_LEFT = atoi(v);
         sprintf(resultBuf, "%d", CONTROL_LEFT);
-      } else if (strcmp(k, "X_DEADZONE") == 0) {
+      } else if(strcmp_P(k, SETTING[S_X_DEADZONE])==0) {
         X_DEADZONE = abs(atoi(v));
         sprintf(resultBuf, "%d", X_DEADZONE);
-      } else if (strcmp(k, "CONTROL_UP") == 0) {
+      } else if (strcmp_P(k, SETTING[S_CONTROL_UP]) == 0) {
         CONTROL_UP = atoi(v);
         sprintf(resultBuf, "%d", CONTROL_UP);
-      } else if (strcmp(k, "CONTROL_CENTER_Y") == 0) {
+      } else if (strcmp_P(k, SETTING[S_CONTROL_CENTER_Y]) == 0) {
         CONTROL_CENTER_Y = atoi(v);
         sprintf(resultBuf, "%d", CONTROL_CENTER_Y);
-      } else if (strcmp(k, "CONTROL_DOWN") == 0) {
+      } else if (strcmp_P(k, SETTING[S_CONTROL_DOWN]) == 0) {
         CONTROL_DOWN = atoi(v);
         sprintf(resultBuf, "%d", CONTROL_DOWN);
-      } else if (strcmp(k, "Y_DEADZONE") == 0) {
+      } else if (strcmp_P(k, SETTING[S_Y_DEADZONE]) == 0) {
         Y_DEADZONE = abs(atoi(v));
         sprintf(resultBuf, "%d", Y_DEADZONE);
-      } else if (strcmp(k, "ACCELERATION_FORWARD") == 0) {
+      } else if (strcmp_P(k, SETTING[S_ACCELERATION_FORWARD]) == 0) {
         ACCELERATION_FORWARD = abs(atof(v));
         dtostrf(ACCELERATION_FORWARD, 0, 4, resultBuf);
-      } else if (strcmp(k, "DECELERATION_FORWARD") == 0) {
+      } else if (strcmp_P(k, SETTING[S_DECELERATION_FORWARD]) == 0) {
         DECELERATION_FORWARD = abs(atof(v));
         dtostrf(DECELERATION_FORWARD, 0, 4, resultBuf);
-      } else if (strcmp(k, "ACCELERATION_BACKWARD") == 0) {
+      } else if (strcmp_P(k, SETTING[S_ACCELERATION_BACKWARD]) == 0) {
         ACCELERATION_BACKWARD = abs(atof(v));
         dtostrf(ACCELERATION_BACKWARD, 0, 4, resultBuf);
-      } else if (strcmp(k, "DECELERATION_BACKWARD") == 0) {
+      } else if (strcmp_P(k, SETTING[S_DECELERATION_BACKWARD]) == 0) {
         DECELERATION_BACKWARD = abs(atof(v));
         dtostrf(DECELERATION_BACKWARD, 0, 4, resultBuf);
-      } else if (strcmp(k, "ACCELERATION_TURNING") == 0) {
+      } else if (strcmp_P(k, SETTING[S_ACCELERATION_TURNING]) == 0) {
         ACCELERATION_TURNING = abs(atof(v));
         dtostrf(ACCELERATION_TURNING, 0, 4, resultBuf);
-      } else if (strcmp(k, "DECELERATION_TURNING") == 0) {
+      } else if (strcmp_P(k, SETTING[S_DECELERATION_TURNING]) == 0) {
         DECELERATION_TURNING = abs(atof(v));
         dtostrf(DECELERATION_TURNING, 0, 4, resultBuf);
-      } else if (strcmp(k, "FASTEST_FORWARD") == 0) {
+      } else if (strcmp_P(k, SETTING[S_FASTEST_FORWARD]) == 0) {
         FASTEST_FORWARD = constrain(abs(atof(v)), 0, 1);
         dtostrf(FASTEST_FORWARD, 0, 4, resultBuf);
-      } else if (strcmp(k, "FASTEST_BACKWARD") == 0) {
+      } else if (strcmp_P(k, SETTING[S_FASTEST_BACKWARD]) == 0) {
         FASTEST_BACKWARD = constrain(abs(atof(v)), 0, 1);
         dtostrf(FASTEST_BACKWARD, 0, 4, resultBuf);
-      } else if (strcmp(k, "TURN_SPEED") == 0) {
+      } else if (strcmp_P(k, SETTING[S_TURN_SPEED]) == 0) {
         TURN_SPEED = constrain(abs(atof(v)), 0, 1);
         dtostrf(TURN_SPEED, 0, 4, resultBuf);
-      } else if (strcmp(k, "SCALE_TURNING_WHEN_MOVING") == 0) {
+      } else if (strcmp_P(k, SETTING[S_SCALE_TURNING_WHEN_MOVING]) == 0) {
         SCALE_TURNING_WHEN_MOVING = atof(v);
         dtostrf(SCALE_TURNING_WHEN_MOVING, 0, 4, resultBuf);
-      } else if (strcmp(k, "SCALE_ACCEL_WITH_SPEED") == 0) {
+      } else if (strcmp_P(k, SETTING[S_ACCELERATION_TURNING]) == 0) {
         SCALE_ACCEL_WITH_SPEED = atoi(v);
         if (SCALE_ACCEL_WITH_SPEED)
           sprintf(resultBuf, "true");
         else
           sprintf(resultBuf, "false");
-      } else if (strcmp(k, "REVERSE_TURN_IN_REVERSE") == 0) {
+      } else if (strcmp_P(k, SETTING[S_REVERSE_TURN_IN_REVERSE]) == 0) {
         REVERSE_TURN_IN_REVERSE = atoi(v);
         if (REVERSE_TURN_IN_REVERSE)
           sprintf(resultBuf, "true");
         else
           sprintf(resultBuf, "false");
-      } else if (strcmp(k, "LEFT_MOTOR_CENTER") == 0) {
+      } else if (strcmp_P(k, SETTING[S_ENABLE_STARTUP_PULSE]) == 0) {
         LEFT_MOTOR_CENTER = atoi(v);
         sprintf(resultBuf, "%d", LEFT_MOTOR_CENTER);
-      } else if (strcmp(k, "LEFT_MOTOR_SLOW") == 0) {
+      } else if (strcmp_P(k, SETTING[S_LEFT_MOTOR_CENTER]) == 0) {
         LEFT_MOTOR_SLOW = atoi(v);
         sprintf(resultBuf, "%d", LEFT_MOTOR_SLOW);
-      } else if (strcmp(k, "LEFT_MOTOR_FAST") == 0) {
+      } else if (strcmp_P(k, SETTING[S_LEFT_MOTOR_SLOW]) == 0) {
         LEFT_MOTOR_FAST = atoi(v);
         sprintf(resultBuf, "%d", LEFT_MOTOR_FAST);
-      } else if (strcmp(k, "RIGHT_MOTOR_CENTER") == 0) {
+      } else if (strcmp_P(k, SETTING[S_LEFT_MOTOR_FAST]) == 0) {
         RIGHT_MOTOR_CENTER = atoi(v);
         sprintf(resultBuf, "%d", RIGHT_MOTOR_CENTER);
-      } else if (strcmp(k, "RIGHT_MOTOR_SLOW") == 0) {
+      } else if (strcmp_P(k, SETTING[S_RIGHT_MOTOR_CENTER]) == 0) {
         RIGHT_MOTOR_SLOW = atoi(v);
         sprintf(resultBuf, "%d", RIGHT_MOTOR_SLOW);
-      } else if (strcmp(k, "RIGHT_MOTOR_FAST") == 0) {
+      } else if (strcmp_P(k, SETTING[S_RIGHT_MOTOR_SLOW]) == 0) {
         RIGHT_MOTOR_FAST = atoi(v);
         sprintf(resultBuf, "%d", RIGHT_MOTOR_FAST);
-      } else if (strcmp(k, "USE_SPEED_KNOB") == 0) {
+      } else if (strcmp_P(k, SETTING[S_RIGHT_MOTOR_FAST]) == 0) {
         USE_SPEED_KNOB = atoi(v);
         if (USE_SPEED_KNOB)
           sprintf(resultBuf, "true");
         else
           sprintf(resultBuf, "false");
-      } else if (strcmp(k, "SPEED_KNOB_SLOW_VAL") == 0) {
+      } else if (strcmp_P(k, SETTING[S_USE_SPEED_KNOB]) == 0) {
         SPEED_KNOB_SLOW_VAL = atoi(v);
         sprintf(resultBuf, "%d", SPEED_KNOB_SLOW_VAL);
-      } else if (strcmp(k, "SPEED_KNOB_FAST_VAL") == 0) {
+      } else if (strcmp_P(k, SETTING[S_SPEED_KNOB_SLOW_VAL]) == 0) {
         SPEED_KNOB_FAST_VAL = atoi(v);
         sprintf(resultBuf, "%d", SPEED_KNOB_FAST_VAL);
-      } else if (strcmp(k, "JOY_X_PIN") == 0) {
+      } else if (strcmp_P(k, SETTING[S_SPEED_KNOB_FAST_VAL]) == 0) {
         JOY_X_PIN = atoi(v);
         pinMode(JOY_X_PIN, INPUT);
         sprintf(resultBuf, "%d", JOY_X_PIN);
-      } else if (strcmp(k, "JOY_Y_PIN") == 0) {
+      } else if (strcmp_P(k, SETTING[S_SPEED_KNOB_PIN]) == 0) {
         JOY_Y_PIN = atoi(v);
         pinMode(JOY_Y_PIN, INPUT);
         sprintf(resultBuf, "%d", JOY_Y_PIN);
---
+++ gbg_program/settings_names.h
@@ -0,0 +1,109 @@
+#ifndef SETTINGS_NAMES_H
+#define SETTINGS_NAMES_H
+
+#define FV(x) (__FlashStringHelper*)(x)
+
+const char control_right[] PROGMEM = "CONTROL_RIGHT";
+const char control_center_x[] PROGMEM = "CONTROL_CENTER_X";
+const char control_left[] PROGMEM = "CONTROL_LEFT";
+const char x_deadzone[] PROGMEM = "X_DEADZONE";
+const char control_up[] PROGMEM = "CONTROL_UP";
+const char control_center_y[] PROGMEM = "CONTROL_CENTER_Y";
+const char control_down[] PROGMEM = "CONTROL_DOWN";
+const char y_deadzone[] PROGMEM = "Y_DEADZONE";
+const char acceleration_forward[] PROGMEM = "ACCELERATION_FORWARD";
+const char deceleration_forward[] PROGMEM = "DECELERATION_FORWARD";
+const char acceleration_backward[] PROGMEM = "ACCELERATION_BACKWARD";
+const char deceleration_backward[] PROGMEM = "DECELERATION_BACKWARD";
+const char acceleration_turning[] PROGMEM = "ACCELERATION_TURNING";
+const char deceleration_turning[] PROGMEM = "DECELERATION_TURNING";
+const char fastest_forward[] PROGMEM = "FASTEST_FORWARD";
+const char fastest_backward[] PROGMEM = "FASTEST_BACKWARD";
+const char turn_speed[] PROGMEM = "TURN_SPEED";
+const char scale_turning_when_moving[] PROGMEM = "SCALE_TURNING_WHEN_MOVING";
+const char reverse_turn_in_reverse[] PROGMEM = "REVERSE_TURN_IN_REVERSE";
+const char left_motor_center[] PROGMEM = "LEFT_MOTOR_CENTER";
+const char left_motor_slow[] PROGMEM = "LEFT_MOTOR_SLOW";
+const char left_motor_fast[] PROGMEM = "LEFT_MOTOR_FAST";
+const char right_motor_center[] PROGMEM = "RIGHT_MOTOR_CENTER";
+const char right_motor_slow[] PROGMEM = "RIGHT_MOTOR_SLOW";
+const char right_motor_fast[] PROGMEM = "RIGHT_MOTOR_FAST";
+const char use_speed_knob[] PROGMEM = "USE_SPEED_KNOB";
+const char speed_knob_slow_val[] PROGMEM = "SPEED_KNOB_SLOW_VAL";
+const char speed_knob_fast_val[] PROGMEM = "SPEED_KNOB_FAST_VAL";
+const char scale_accel_with_speed[] PROGMEM = "SCALE_ACCEL_WITH_SPEED";
+const char speed_knob_pin[] PROGMEM = "SPEED_KNOB_PIN";
+const char joy_x_pin[] PROGMEM = "JOY_X_PIN";
+const char joy_y_pin[] PROGMEM = "JOY_Y_PIN";
+
+const char* SETTING[] PROGMEM = {
+  control_right,
+  control_center_x,
+  control_left,
+  x_deadzone,
+  control_up,
+  control_center_y,
+  control_down,
+  y_deadzone,
+  acceleration_forward,
+  deceleration_forward,
+  acceleration_backward,
+  deceleration_backward,
+  acceleration_turning,
+  deceleration_turning,
+  fastest_forward,
+  fastest_backward,
+  turn_speed,
+  scale_turning_when_moving,
+  reverse_turn_in_reverse,
+  left_motor_center,
+  left_motor_slow,
+  left_motor_fast,
+  right_motor_center,
+  right_motor_slow,
+  right_motor_fast,
+  use_speed_knob,
+  speed_knob_slow_val,
+  speed_knob_fast_val,
+  scale_accel_with_speed,
+  speed_knob_pin,
+  joy_x_pin,
+  joy_y_pin
+};
+
+enum {
+  S_CONTROL_RIGHT = 0,
+  S_CONTROL_CENTER_X,
+  S_CONTROL_LEFT,
+  S_X_DEADZONE,
+  S_CONTROL_UP,
+  S_CONTROL_CENTER_Y,
+  S_CONTROL_DOWN,
+  S_Y_DEADZONE,
+  S_ACCELERATION_FORWARD,
+  S_DECELERATION_FORWARD,
+  S_ACCELERATION_BACKWARD,
+  S_DECELERATION_BACKWARD,
+  S_ACCELERATION_TURNING,
+  S_DECELERATION_TURNING,
+  S_FASTEST_FORWARD,
+  S_FASTEST_BACKWARD,
+  S_TURN_SPEED,
+  S_SCALE_TURNING_WHEN_MOVING,
+  S_REVERSE_TURN_IN_REVERSE,
+  S_LEFT_MOTOR_CENTER,
+  S_LEFT_MOTOR_SLOW,
+  S_LEFT_MOTOR_FAST,
+  S_RIGHT_MOTOR_CENTER,
+  S_RIGHT_MOTOR_SLOW,
+  S_RIGHT_MOTOR_FAST,
+  S_USE_SPEED_KNOB,
+  S_SPEED_KNOB_SLOW_VAL,
+  S_SPEED_KNOB_FAST_VAL,
+  S_SCALE_ACCEL_WITH_SPEED,
+  S_SPEED_KNOB_PIN,
+  S_JOY_X_PIN,
+  S_JOY_Y_PIN
+};
+
+#endif // SETTINGS_NAMES_H
