15a6a1f43fc22882479b10db40c6bcde8633e98c
joshua-8 <joshua262144@gmail.com>
2025-06-15 03:15:12 -0700

    fix variable name conflicts in input processor file

--- gbg_program/_Input_Processors.ino
+++ gbg_program/_Input_Processors.ino
@@ -1,15 +1,15 @@
 // returns a value between 0 and 1 after reading a knob
-float InputProcessor_ReadKnob(byte SPEED_KNOB_PIN, int slowVal, int fastVal, int& raw) {
-  raw = analogRead(SPEED_KNOB_PIN);
+float InputProcessor_ReadKnob(byte _SPEED_KNOB_PIN, int slowVal, int fastVal, int& raw) {
+  raw = analogRead(_SPEED_KNOB_PIN);
   return constrain(floatMap(raw, slowVal, fastVal, 0, 1), 0, 1);
 }
 
-void InputProcessor_ScaleInput(float speedKnobScaler, float &turnInput, float &speedInput, float FASTEST_FORWARD, float FASTEST_BACKWARD, float TURN_SPEED) {
+void InputProcessor_ScaleInput(float speedKnobScaler, float &turnInput, float &speedInput, float _FASTEST_FORWARD, float FASTEST_BACKWARD, float TURN_SPEED) {
   FASTEST_BACKWARD = abs(FASTEST_BACKWARD);
-  FASTEST_FORWARD = abs(FASTEST_FORWARD);
+  _FASTEST_FORWARD = abs(_FASTEST_FORWARD);
   TURN_SPEED = abs(TURN_SPEED);
-  speedInput = twoMap(speedInput, -1, 0, 1, /**/ -FASTEST_BACKWARD * speedKnobScaler, 0, 0, FASTEST_FORWARD * speedKnobScaler, 0);
-  speedInput = constrain(speedInput, -FASTEST_BACKWARD * speedKnobScaler, FASTEST_FORWARD * speedKnobScaler);
+  speedInput = twoMap(speedInput, -1, 0, 1, /**/ -FASTEST_BACKWARD * speedKnobScaler, 0, 0, _FASTEST_FORWARD * speedKnobScaler, 0);
+  speedInput = constrain(speedInput, -FASTEST_BACKWARD * speedKnobScaler, _FASTEST_FORWARD * speedKnobScaler);
 
   turnInput = constrain(turnInput * TURN_SPEED * speedKnobScaler, -TURN_SPEED * speedKnobScaler, TURN_SPEED * speedKnobScaler);
 }
@@ -19,20 +19,20 @@ void InputProcessor_ScaleInput(float speedKnobScaler, float &turnInput, float &s
               multipliying all accleration and deceleration values by scaler keeps time to max speed constant instead of acceleration being constant if a speed knob is being used to scale speed
 */
 
-float InputProcessor_LimitAccelerationFourSettings(float velocity, float velocityTarget, float scaler, float ACCELERATION_FORWARD, float DECELERATION_FORWARD, float ACCELERATION_BACKWARD, float DECELERATION_BACKWARD, float timeInterval) {
-  ACCELERATION_FORWARD = max(ACCELERATION_FORWARD, (float)0.0) * scaler;
-  DECELERATION_FORWARD = max(DECELERATION_FORWARD, (float)0.0) * scaler;
-  ACCELERATION_BACKWARD = max(ACCELERATION_BACKWARD, (float)0.0) * scaler;
-  DECELERATION_BACKWARD = max(DECELERATION_BACKWARD, (float)0.0) * scaler;
+float InputProcessor_LimitAccelerationFourSettings(float velocity, float velocityTarget, float scaler, float ACCELERATION__FORWARD, float DECELERATION__FORWARD, float ACCELERATION__BACKWARD, float DECELERATION__BACKWARD, float timeInterval) {
+  ACCELERATION__FORWARD = max(ACCELERATION__FORWARD, (float)0.0) * scaler;
+  DECELERATION__FORWARD = max(DECELERATION__FORWARD, (float)0.0) * scaler;
+  ACCELERATION__BACKWARD = max(ACCELERATION__BACKWARD, (float)0.0) * scaler;
+  DECELERATION__BACKWARD = max(DECELERATION__BACKWARD, (float)0.0) * scaler;
   if (velocity == 0) {
-    velocity += constrain(velocityTarget - velocity, -ACCELERATION_BACKWARD * timeInterval, ACCELERATION_FORWARD * timeInterval);
+    velocity += constrain(velocityTarget - velocity, -ACCELERATION__BACKWARD * timeInterval, ACCELERATION__FORWARD * timeInterval);
   } else if (velocity > 0) {
-    velocity += constrain(velocityTarget - velocity, -DECELERATION_FORWARD * timeInterval, ACCELERATION_FORWARD * timeInterval);
+    velocity += constrain(velocityTarget - velocity, -DECELERATION__FORWARD * timeInterval, ACCELERATION__FORWARD * timeInterval);
     if (velocity < 0) {  // prevent decel from crossing zero and causing accel
       velocity = 0;
     }
   } else {  //velocity < 0
-    velocity += constrain(velocityTarget - velocity, -ACCELERATION_BACKWARD * timeInterval, DECELERATION_BACKWARD * timeInterval);
+    velocity += constrain(velocityTarget - velocity, -ACCELERATION__BACKWARD * timeInterval, DECELERATION__BACKWARD * timeInterval);
     if (velocity > 0) {  // prevent decel from crossing zero and causing accel
       velocity = 0;
     }
