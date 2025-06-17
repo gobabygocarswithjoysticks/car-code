5cc6a40ef7f3e0e3d39e8e058d3a3291ceddad20
joshua-8 <joshua262144@gmail.com>
2025-06-17 13:20:03 -0700

    rc timeout

--- gbg_program/gbg_program.ino
+++ gbg_program/gbg_program.ino
@@ -429,7 +429,7 @@ const byte settings_memory_key = 18;
 #endif
 #endif
 
-#define rcTimeoutMicros 2500000 // timeout for RC control
+#define rcTimeoutMicros 50000 // timeout for RC control
 #if defined(IS_PICO) || defined(ESP32)
 #else
 #include <PinChangeInterrupt.h>
